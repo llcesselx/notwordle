6a30eff3ad9137552a4d76999e27cf6980de71e5
Llcesselx <ltice8@icloud.com>
2022-12-06 10:59:45 -0500

    final

--- game.js
+++ game.js
@@ -1,3 +1,9 @@
-function startGame() {
-    console.log("game starting...")
+// /^[a-zA-Z]$/ is something I learned from frontend masters. The stuff in
+// the / / is a regular expression and is a data type in Javascript.
+function isLetter(letter) {
+    return /^[a-zA-Z]$/.test(letter);
 }
+
+console.log(isLetter("a"));
+console.log(isLetter("abc"));
+console.log(isLetter("1"));
